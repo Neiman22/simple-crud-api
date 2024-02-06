1474013440cc68f06225ff5cd64dd8ce818a06ba
Neiman22 <pushkarev_av@inbox.ru>
2024-02-06 12:06:33 +0300

    fix: add GET api/users/{userId}

--- src/data.ts
+++ src/data.ts
@@ -20,3 +20,12 @@ addUser('Artem', 39, ['skis']);
 export const getAllUsers = () => {
   return users;
 }
+
+export const getUserByID = (id: string) => {
+  return users.find(user => user.id === id);
+}
+
+export const validateUserId = (userId: string) => {
+  const uuidRegex = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+  return uuidRegex.test(userId);
+}
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,43 @@
 import { createServer } from 'http';
-import { getAllUsers } from './data';
+import { getAllUsers, getUserByID, validateUserId } from './data';
 
 export const server = createServer(async (req, res) => {
+  const { method, url } = req;
 
-  if (req.method === 'GET' && req.url === '/api/users') {
-    const users = getAllUsers();
-    console.log(users);
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(users));
+  if (method === 'GET' && url === '/api/users') {
+    try {
+      const users = getAllUsers();
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(users));
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ error: 'Internal server error' }));
+    }
   }
-  
+
+  if (method === 'GET' && url?.startsWith('/api/users/')) {
+    const userId = url.split('/')[3];
+    if (!validateUserId(userId)) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ error: 'Invalid userId' }));
+      return;
+    }
+
+    try {
+      const user = getUserByID(userId);
+      if (!user) {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'User not found' }));
+        return;
+      }
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(user));
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ error: 'Internal server error' }));
+    }
+
+    
+  }
 }) 
