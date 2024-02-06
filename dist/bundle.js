2df23899557abf5d6959582bae61030cf725ac05
Neiman22 <pushkarev_av@inbox.ru>
2024-02-06 10:13:00 +0300

    fix: add start:dev and start:prod

---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+console.log('Hello, TypeScript!');
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import dotenv from 'dotenv';
-import { v4 as uuidv4 } from 'uuid';
-
-dotenv.config();
-
 console.log('Hello, TypeScript!');
-console.log('Generated UUID:', uuidv4());
-console.log('Environment variable EXAMPLE:', process.env.EXAMPLE);
